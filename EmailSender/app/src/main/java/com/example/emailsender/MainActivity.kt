448ee8e6e21dcd70a3693a37e04fafe6acc4d9fa
Egor <brazkhin.e92@gmail.com>
2022-01-05 11:45:35 +0500

    firstCommit

---
+++ EmailSender/app/src/main/java/com/example/emailsender/MainActivity.kt
@@ -0,0 +1,42 @@
+package com.example.emailsender
+
+import android.content.Intent
+import android.net.Uri
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
+import java.lang.Exception
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        sendEmailBtn.setOnClickListener {
+
+            val recipient = recipientEt.text.toString().trim()
+            val subject = subjectEt.text.toString().trim()
+            val message = messageEt.text.toString().trim()
+
+            sendEmail(recipient, subject, message)
+        }
+    }
+
+    private fun sendEmail(recipient: String, subject: String, message: String) {
+        val mIntent = Intent(Intent.ACTION_SEND)
+
+        mIntent.data = Uri.parse("mailto:")
+        mIntent.type = "text/plain"
+        mIntent.putExtra(Intent.EXTRA_EMAIL, arrayOf(recipient))
+        mIntent.putExtra(Intent.EXTRA_SUBJECT, subject)
+        mIntent.putExtra(Intent.EXTRA_TEXT, message)
+
+        try {
+            startActivity(Intent.createChooser(mIntent,"Choose Email Client..."))
+        }
+        catch (e: Exception) {
+            Toast.makeText(this, e.message, Toast.LENGTH_LONG).show()
+        }
+    }
+}
